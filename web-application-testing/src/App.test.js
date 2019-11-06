b847c6949007257ac895e5347e15d5b854e8bc89
patrick-replogle <patrickr1138@gmail.com>
2019-11-05 19:01:36 -0800

    testing dependecies added. Beginnging to toy with tests.

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -1,13 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 afterEach(rtl.cleanup);
 
-// import App from "./App";
+import App from "./App";
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
