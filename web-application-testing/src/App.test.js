55d72affacd90e15ac0adcfe8c6b0ed99d53ec4e
patrick-replogle <patrickr1138@gmail.com>
2019-11-05 20:17:27 -0800

    MVP complete.

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -5,11 +5,19 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import Display from "./components/Display";
 import Dashboard from "./components/Dashboard";
+import { addBall, addStrike, addFoul, addHit } from "./components/functions";
 
 afterEach(rtl.cleanup);
 
 test("App renders without crashing", () => {
   rtl.render(<App />);
+});
+
+test("App is a thing", () => {
+  const n = <App />;
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeFalsy();
 });
 
 test("Display renders without crashing", () => {
@@ -22,6 +30,35 @@ test('renders "Chicago Cubs" text', () => {
   expect(wrapper.getByText(/chicago cubs/i));
 });
 
+test('renders "Balls" text', () => {
+  const wrapper = rtl.render(<App />);
+
+  expect(wrapper.getByText(/balls/i));
+});
+
+test('renders "Strikes" text', () => {
+  const wrapper = rtl.render(<App />);
+
+  expect(wrapper.getByText(/strikes/i));
+});
+
 test("Dashboard renders without crashing", () => {
   rtl.render(<Dashboard />);
+});
+
+test("addball works", () => {
+  expect(addBall(0)).toBe(1);
+  expect(addBall(3)).toBe(4);
+});
+
+test("addstrike works", () => {
+  expect(addStrike(0)).toBe(1);
+});
+
+test("addFoul works", () => {
+  expect(addFoul(0)).toBe(1);
+});
+
+test("addBall works", () => {
+  expect(addBall(0)).toBe(1);
 });
